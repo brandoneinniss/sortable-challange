2c3807dd9f0b62730672dc7bd3e90b040b9b65e7
brandoneinniss <44344419+brandoneinniss@users.noreply.github.com>
2020-05-20 02:46:42 -0400

    bidder_factory included comments

--- auction/bidder_factory.py
+++ auction/bidder_factory.py
@@ -6,6 +6,7 @@ from util.config_parser import BidderData
 class BidderFactory:
     @staticmethod
     def create_bidder_dict(bidders_data: Dict[str, BidderData]):
+        """Creates a dictionary of bidder objects and returns a dictionary"""
         bidder_object_dict = {}
         for key, value in bidders_data.items():
             bidder_object_dict[key] =\
@@ -14,6 +15,7 @@ class BidderFactory:
 
     @staticmethod
     def create_bidder(bidder_data: Bidder):
+        """Creates a bidder object and returns a bidder"""
         name = bidder_data.name
         adjustment = bidder_data.adjustment
         return Bidder(name, adjustment)
