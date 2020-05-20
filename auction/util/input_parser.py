3a331fd3c251441e88ce05bd22196722424ce7af
brandoneinniss <44344419+brandoneinniss@users.noreply.github.com>
2020-05-20 03:00:14 -0400

    input_parser included comments

--- auction/util/input_parser.py
+++ auction/util/input_parser.py
@@ -10,12 +10,14 @@ class InputParser:
 
     @staticmethod
     def parse(input_path):
+        """Parse method to parse input json file"""
         input_data = get_json_file_content(input_path)
         if input_path:
             return InputData(InputParser._parse_input(input_data))
 
     @staticmethod
     def _parse_input(data):
+        """Prepare input data from file data"""
         auctions_object = []
         for auction in data:
             site = auction['site']
@@ -28,6 +30,7 @@ class InputParser:
 
     @staticmethod
     def _parse_units(units):
+        """Prepare units from file data"""
         unit_values = []
         for unit in units:
             unit_values.append(unit)
@@ -35,6 +38,7 @@ class InputParser:
 
     @staticmethod
     def _parse_bids(bids):
+        """Prepare bids from file data"""
         bids_object = []
         for bid in bids:
             bidder = bid['bidder']
